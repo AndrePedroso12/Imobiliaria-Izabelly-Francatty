23c0e57cd978b1ec64163b86c0f923d5dfb9ebe0
Bruno Dias <brunoadias1996@gmail.com>
2024-05-07 20:19:10 -0300

    ajustes

--- server/routes/api.php
+++ server/routes/api.php
@@ -16,8 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/teste', [UserController::class, 'createUser']);
-
 Route::post('/login', [UserController::class, 'login']);
 
 Route::middleware(CheckToken::class, CheckAdmin::class)->group(function () {
