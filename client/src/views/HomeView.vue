23d9d3bfa579f99f5d6f6520ea011be2f424cda7
Andre Pedroso <editorcle@gmail.com>
2024-05-09 23:42:41 -0300

    configurado funções da API

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -11,10 +11,38 @@ import BarraContato from '@/components/Shared/BarraContato.vue'
 import NuvemDeTags from '@/components/Shared/NuvemDeTags.vue'
 
 import { ImoveisTeste } from '../components/Shared/dataTest.js'
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+
+import { useAuth, useImoveis } from '@/composables'
 
 const ImoviesRef = ref(ImoveisTeste)
+const ImoveisFunction = useImoveis()
+const loginFunction = useAuth()
+let token =
+  'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7ImlkIjoxLCJuYW1lIjoiQW5kcmVQZWRyb3NvIiwiZW1haWwiOiJBbmRyZVBlZHJvc28iLCJlbWFpbF92ZXJpZmllZF9hdCI6bnVsbCwiY3JlYXRlZF9hdCI6IjIwMjQtMDUtMDlUMjI6NTQ6MDcuMDAwMDAwWiIsInVwZGF0ZWRfYXQiOiIyMDI0LTA1LTA5VDIyOjU0OjA3LjAwMDAwMFoiLCJhZG1pbiI6MSwiZGVsZXRlZF9hdCI6bnVsbH0sImlwIjoiMTI3LjAuMC4xIn0.uymBUyICaZbJaw1uEX-flt-ePtBkiVKMZemL4O0t42I'
+// Função para carregar todos os imóveis
 
+async function carregarViaComposable() {
+  try {
+    const data = await ImoveisFunction.carregarImoveis()
+    console.log('Imóveis carregados:', data)
+  } catch (error) {
+    console.error('Erro ao carregar imóveis:', error)
+  }
+}
+async function LoginTeste() {
+  try {
+    const data = await loginFunction.login('AndrePedroso', 'Canola12')
+    console.log('Imóveis carregados:', data)
+  } catch (error) {
+    console.error('Erro ao carregar imóveis:', error)
+  }
+}
+
+onMounted(async () => {
+  await LoginTeste()
+  carregarViaComposable()
+})
 const banners = ImoviesRef.value.filter((obj) => obj.banner === true)
 const maisBuscados = ImoviesRef.value.filter((obj) => obj.isfavourite === true)
 const novosImoveis = ImoviesRef.value.filter(
