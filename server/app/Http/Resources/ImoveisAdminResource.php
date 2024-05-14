110418704bb2b5e591577b20ed8d34b16bdde382
Bruno Dias <brunoadias1996@gmail.com>
2024-05-14 20:20:42 -0300

    ajuste nos resources

--- server/app/Http/Resources/ImoveisAdminResource.php
+++ server/app/Http/Resources/ImoveisAdminResource.php
@@ -30,7 +30,7 @@ class ImoveisAdminResource extends JsonResource
             'category' => $this->imovel->tipo,
             'model' => $this->imovel->pretensao,
             'mainImage' => $this->imovel->mainImage,
-            'images' => $this->imovel->images,
+            'images' => ImagesResource::collection($this->imovel->images),
             'video' => '/api/imovel/getVideo/' . $this->imovel->id,
             'location' => [
                 'city' => $this->imovel->cidade,
